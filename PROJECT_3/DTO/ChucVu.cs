8a02baeebc838cfd7b9df402d1ea91d8d700ebc6
concac <minhduc2247@gmail.com>
2021-04-13 20:21:24 +0700

    a

---
+++ PROJECT_3/DTO/ChucVu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_3.DTO
+{
+    class ChucVu
+    {
+        public string ID_ChucVu { get; set; }
+        public string TenChucVu { get; set; }
+    }
+}
---
+++ PROJECT_3/DTO/TaiKhoan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_3.DTO
+{
+    class TaiKhoan
+    {
+        public string ID_TaiKhoan { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+    }
+}
